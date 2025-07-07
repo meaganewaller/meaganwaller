9f2eeb35c210a0cf203ceaed14db17cb503e1372
Meagan Waller <meagan@meaganwaller.com>
2025-07-07 18:21:19 -0400

    Add @hotwired/stimulus dependency, implement Stimulus application and example controller

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,6 +1,7 @@
 import "$styles/index.css"
 import "$styles/syntax-highlighting.css"
 import * as Turbo from "@hotwired/turbo"
+import { Application } from "@hotwired/stimulus"
 
 /**
  * Adds support for declarative shadow DOM. Requires your HTML <head> to include:
@@ -19,3 +20,17 @@ import * as TurboShadow from "turbo-shadow"
 import components from "$components/**/*.{js,jsx,js.rb,css}"
 
 console.info("Bridgetown is loaded!")
+
+window.Stimulus = Application.start()
+
+import controllers from "./controllers/**/*.{js,js.rb}"
+Object.entries(controllers).forEach(([filename, controller]) => {
+  if (filename.includes("_controller.") || filename.includes("-controller.")) {
+    const identifier = filename.replace("./controllers/", "")
+      .replace(/[_-]controller\..*$/, "")
+      .replace(/_/g, "-")
+      .replace(/\//g, "--")
+
+    Stimulus.register(identifier, controller.default)
+  }
+})
